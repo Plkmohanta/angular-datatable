e17ab3371102a5efdd572988a4ed1cbcaf12d002
Plkmohanta <30947118+Plkmohanta@users.noreply.github.com>
2019-10-29 16:22:58 +0530

    dataTable ,pagination,sort added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { AppService, PeriodicElement } from './app.service';
-import { PageEvent } from '@angular/material/paginator';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AppService, DataModel } from './app.service';
+import {  MatPaginator } from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
+
 
 @Component({
   selector: 'app-root',
@@ -8,20 +11,21 @@ import { PageEvent } from '@angular/material/paginator';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource: PeriodicElement;
-  length = 100;
-  pageSize = 10;
-  pageSizeOptions: number[] = [5, 10, 25, 100];
-  pageEvent: PageEvent;
+  displayedColumns: string[] = ['id', 'title', 'url', 'thumbnailUrl'];
+  dataSource:MatTableDataSource<DataModel>;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-
-  constructor(private appService: AppService) { }
+  constructor(private appService: AppService) { 
+  }
 
   ngOnInit() {
     this.appService.getAllData().subscribe(
       res => {
-        this.dataSource = res;
+        this.dataSource = new MatTableDataSource<DataModel>(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
 
       },
       err => console.log(err)
@@ -30,10 +34,11 @@ export class AppComponent implements OnInit {
   }
   cellClicked(element) {
     console.log(this.dataSource);
-
   }
 
-  setPageSizeOptions(setPageSizeOptionsInput: string) {
-    this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
+  
 }
